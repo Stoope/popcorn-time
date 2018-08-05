7412a2ffbda000c035d0fe0475d5c3f7d26a1fc7
Stepan Kurennykh <stoope@outlook.com>
2018-08-05 15:57:04 +0300

    fix request

--- src/app/request/index.tsx
+++ src/app/request/index.tsx
@@ -2,6 +2,6 @@ import axios, { AxiosRequestConfig } from 'axios';
 import store from '~/HOCs/ReduxWithMidleware/store';
 
 const request = (config: AxiosRequestConfig) =>
-  axios({ proxy: store.getState().settingsReducer.config.proxy, ...config });
+  axios({ proxy: store ? store.getState().settingsReducer.config.proxy : false, ...config });
 
 export default request;
