eadf305aa0fea81b9ff862099fbb45289e580107
Stepan Kurennykh <s.kurennykh@e-mba.ru>
2018-08-07 15:06:48 +0300

    custom scroll bar

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -6,6 +6,23 @@ import { withStyles, StyleRulesCallback } from '@material-ui/core/styles';
 import { Route, Switch } from 'react-router';
 
 const styles: StyleRulesCallback = theme => ({
+  '@global': {
+    '::-webkit-scrollbar': {
+      width: 8,
+      height: 8,
+      borderRadius: 8
+    },
+    '::-webkit-scrollbar-button': {
+      display: 'none'
+    },
+    '::-webkit-scrollbar-track': {
+      background: theme.palette.type === 'light' ? '#e1e1e1' : '#212121'
+    },
+    '::-webkit-scrollbar-thumb': {
+      background: theme.palette.type === 'light' ? '#757575' : '#9e9e9e',
+      borderRadius: 8
+    }
+  },
   root: {
     flexGrow: 1,
     zIndex: 1,
