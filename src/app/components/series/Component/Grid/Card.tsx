f9e0efd4e2a058128b4ec6049b5d761e614b4d40
Stepan Kurennykh <s.kurennykh@e-mba.ru>
2018-08-08 09:33:38 +0300

    seasons

--- src/app/components/series/Component/Grid/Card.tsx
+++ src/app/components/series/Component/Grid/Card.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { State } from 'types';
 import Card from '@material-ui/core/Card';
+import messages from '../index.messages';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { withStyles, StyleRulesCallback } from '@material-ui/core/styles';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
 
 type Props = {
   item: State['seriesReducer']['data'][0];
   classes: Record<string, string>;
-};
+} & InjectedIntlProps;
 
 const styles: StyleRulesCallback = () => ({
   media: {
@@ -28,34 +30,46 @@ class CardComponent extends React.Component<Props> {
       item: {
         title,
         images: { poster },
-        year
+        year,
+        num_seasons
       },
-      classes
+      classes,
+      intl
     } = this.props;
     return (
       <Card className={classes.card}>
         <CardMedia className={classes.media} image={poster} title={title} />
-        <CardContent><Grid container={true} spacing={8}>
-          <Grid item={true} xs={12}>
-            <Typography title={title} noWrap={true} variant="subheading">
-              {title}
-            </Typography>
+        <CardContent>
+          <Grid container={true} spacing={8}>
+            <Grid item={true} xs={12}>
+              <Typography title={title} noWrap={true} variant="subheading">
+                {title}
+              </Typography>
+            </Grid>
+            <Grid item={true} xs={6}>
+              <Typography title={year} noWrap={true} variant="caption">
+                {year}
+              </Typography>
+            </Grid>
+            <Grid item={true} xs={6}>
+              <Typography
+                title={intl.formatMessage(messages.drawer_series_season, {
+                  seasonsCount: num_seasons
+                })}
+                noWrap={true}
+                variant="caption"
+                align="right"
+              >
+                {intl.formatMessage(messages.drawer_series_season, {
+                  seasonsCount: num_seasons
+                })}
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item={true} xs={6}>
-            <Typography title={year} noWrap={true} variant="caption">
-              {year}
-            </Typography>
-          </Grid>
-          <Grid item={true} xs={6}>
-            <Typography title={year} noWrap={true} variant="caption" align="right">
-              {year}
-            </Typography>
-          </Grid>
-        </Grid>
         </CardContent>
       </Card>
     );
   }
 }
 
-export default withStyles(styles)(CardComponent);
+export default withStyles(styles)(injectIntl(CardComponent));
--- src/app/components/series/Component/index.messages.ts
+++ src/app/components/series/Component/index.messages.ts
@@ -1,20 +1,11 @@
 import { defineMessages } from 'react-intl';
 
 const messages = defineMessages({
-  drawer_Drawer_settings: {
-    id: 'app.Drawer.settings',
-    defaultMessage: 'Settings',
-    description: 'Application settings in drawer menu'
-  },
-  drawer_Drawer_series: {
-    id: 'app.Drawer.series',
-    defaultMessage: 'Series',
-    description: 'Series item in application drawer'
-  },
-  drawer_Drawer_movies: {
-    id: 'app.Drawer.movies',
-    defaultMessage: 'Movies',
-    description: 'Movies item in application drawer'
+  drawer_series_season: {
+    id: 'app.series.season',
+    defaultMessage:
+      '{seasonsCount, number} {seasonsCount, plural, one {season} other {seasons}}',
+    description: 'Form of season word(plural)'
   }
 });
 
